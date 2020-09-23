3beb37de3b87247d22ef6caa10e60756cab71719
Frido <archalois3004@gmail.com>
2020-09-24 00:03:40 +0700

    trying splash screen

--- app/src/main/java/com/example/capstoneproject/ActivityFrido.java
+++ app/src/main/java/com/example/capstoneproject/ActivityFrido.java
@@ -2,13 +2,24 @@ package com.example.capstoneproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class ActivityFrido extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frido);
+
+        int loading_time = 5000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(ActivityFrido.this, MainActivity.class);
+                startActivity(home);
+                finish();
+            }
+        }, loading_time);
     }
 }
